404fab67f0918b02a15eb44dbcf172d9d6e6a56e
jughead32 <parbaked@gmail.com>
2018-07-11 15:48:15 -0700

    first commit of first exercise

---
+++ exercises/variables-data-types/src/app.js
@@ -0,0 +1,19 @@
+// *****************************************
+// Variables/Data Types
+function willReturnAString() {
+    // Create a variable below called "theString" that contains any string:
+    var theString = "Hello My name is Justin."
+    return theString;
+}
+
+function willReturnABoolean() {
+    // Create a variable below called "theBoolean" that contains a boolean value:
+    var theBoolean = false;
+    return theBoolean;
+}
+
+function willReturnANumber() {
+    // Create a variable below called "theNumber" that contains a number value:
+    var theNumber = 2;
+    return theNumber;
+}
